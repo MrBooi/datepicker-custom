3210a55b053722a9bfde0b689b2e69876d5dfe6b
Sibabalwe-Qamata <prof.qamata@gmail.com>
2019-08-20 17:51:11 +0200

    Story#03 :Logic set-up

---
+++ javascipt/datePicker.js
@@ -0,0 +1,17 @@
+
+function datePicker() {
+
+    function isLeapYear(year){
+
+    }
+    function formatedDate(dateToFormat){
+
+    }
+
+    function 
+
+     return {
+        getLeapYear: isLeapYear,
+        getFormatedDate : formatedDate
+    }
+}
