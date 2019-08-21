9954eb2eafec4c0b86bcf47990e5b75fa2635f53
Sibabalwe-Qamata <prof.qamata@gmail.com>
2019-08-21 02:04:08 +0200

    Story#03 :Logic - Added decoupled logic functions

--- javascipt/datePicker.js
+++ javascipt/datePicker.js
@@ -1,17 +1,30 @@
 
 function datePicker() {
-
+    // Returns true or false ... 
     function isLeapYear(year){
-
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
     }
     function formatedDate(dateToFormat){
-
+        let day = dateToFormat.getDate();
+        day < 10 ?  day = '0' + day :  day;
+        let month =dateToFormat.getMonth() + 1;
+        month < 10 ?  month = '0' + month :  month;
+        let year = dateToFormat.getFullYear();
+        return day + ' / ' + month + ' / ' + year;
     }
 
-    function 
-
+    function checkElementPath (path, selector) {
+        for (let i = 0; i < path.length; i++) {
+            if (path[i].classList && path[i].classList.contains(selector)) {
+                return true;
+            }
+        }
+        return false;
+    }
      return {
         getLeapYear: isLeapYear,
-        getFormatedDate : formatedDate
+        getFormatedDate : formatedDate,
+        getElementPath : checkElementPath
+
     }
 }
