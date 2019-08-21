a7a7b76bec927e6961074e5982c9140f3e70aa6c
Sibabalwe-Qamata <prof.qamata@gmail.com>
2019-08-21 02:00:18 +0200

    Story#04 :Unit tests - Added Unit test for leap year check

--- test/datepicker-test.js
+++ test/datepicker-test.js
@@ -1,6 +1,17 @@
-describe('The Date Picker function', function() 
+"use scrict";
+
+describe('The Date Picker functionality Unit Test Cases', function() 
 {
- 
+    it('It should return a boolean value (true) if the year is a leap year ',
+    function() {
+      let dateSelector = datePicker();
+      assert.equal(dateSelector.getLeapYear(2020),true);
+     });
 
+     it('It should return a boolean value (false) if the year is not a leap year ',
+     function() {
+       let dateSelector = datePicker();
+       assert.equal(dateSelector.getLeapYear(2019),false);
+      });
 
-  });
+});
