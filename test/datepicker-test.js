53bbaf660cf7fc4809d6551ce77883c7e3483d82
Sibabalwe-Qamata <prof.qamata@gmail.com>
2019-08-21 12:59:14 +0200

    Story#04 :Unit Tests - Added unit test to check for leap years and return the correct number of days for a month

--- test/datepicker-test.js
+++ test/datepicker-test.js
@@ -13,5 +13,15 @@ describe('The Date Picker functionality Unit Test Cases', function()
        let dateSelector = datePicker();
        assert.equal(dateSelector.getLeapYear(2019),false);
       });
+      it('It should return 29 days for the month of February given a Leap year',
+     function() {
+       let dateSelector = datePicker();
+       assert.equal(dateSelector.getMonthDayNumber(1,2020),'29');
+      });
+      it('It should return 28 days for the month of February given a Leap year',
+     function() {
+       let dateSelector = datePicker();
+       assert.equal(dateSelector.getMonthDayNumber(1,2019),'28');
+      });
 
 });
